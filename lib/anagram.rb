49bcaff1dc1b564f3e9cb60027e81da54111a836
Sara Kantor <sara.kantor.nv@gmail.com>
2020-10-15 21:20:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :list_words
+  
+  def new(word)
+    @word = word
+  end
+  
+  def match(word)
+    final = []
+    @list_words.each do |x|
+      if x.sort == word.sort 
+        final << x 
+      end 
+    end 
+    final
+  end 
+  
+end 
